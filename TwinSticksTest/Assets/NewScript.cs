3507d662e5791c926201260bae2a66329d9c96c3
PADDYPC\Paddy <dssiriusindustry@gmail.com>
2017-12-09 18:21:28 +0000

    Added script

---
+++ TwinSticksTest/Assets/NewScript.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NewScript : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+
+	// I can't get this bleeding thing to work ;(
+
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
