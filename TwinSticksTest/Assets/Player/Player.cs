72f2fef1c5e59e8babe0962976e138af65ac4ecd
PADDYPC\Paddy <dssiriusindustry@gmail.com>
2017-12-11 08:59:58 +0000

    Test to show Fowler what this does

---
+++ TwinSticksTest/Assets/Player/Player.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityStandardAssets.CrossPlatformInput;
+
+public class Player : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+
+	void Update () {
+		print ("Axis: " + CrossPlatformInputManager.GetAxis ("Horizontal"));
+	}
+
+	// Update is called once per frame
+	void FixedUpdate () {
+		if (CrossPlatformInputManager.GetButtonDown ("Fire1")) {
+			Debug.Log ("fire!");
+		}
+	}
+}
